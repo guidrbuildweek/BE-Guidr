d4bd88c8ccc45d57a780bfe71cec53a21fcfab80
Henry Neal <henry.neal0@gmail.com>
2019-03-11 19:02:29 -0400

    all crud firing for profile and trip type

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ const authRoute = require('../auth/authRoute');
 const helmet = require("helmet");
 const cors = require("cors");
 const Trips = require("./tripModel");
+const Profile = require('./profileModel');
 const db = require('../data/dbConfig');
 const server = express();
 
@@ -18,6 +19,8 @@ server.get("/", (req, res) => {
 
 server.use('/', authRoute);
 
+
+// TRIP CRUD START // TRIP CRUD START // TRIP CRUD START // TRIP CRUD START // TRIP CRUD START // TRIP CRUD START 
 //GET //GET //GET //GET //GET //GET //GET //GET //GET //GET //GET //GET //GET //GET //GET //GET //GET //GET //GET //GET 
 
 server.get('/trips', async (req, res) => {
@@ -43,23 +46,6 @@ server.get('/trips/:id', async (req, res) => {
 });
 
 //   Update //   Update //   Update //   Update //   Update //   Update //   Update //   Update //   Update //   Update 
-// server.put('/trips/:id', async (req, res) => {
-//     const{tripType, tripName, durationStart, durationEnd, privPro, datePosted, location,} = req.body;
-//     if(!tripType || !tripName || !durationStart || !durationEnd || !privPro || !datePosted || !location)
-//         return res.status(422).json({message:'you need to provide at least one field to update.'})
-//     try {
-//         const updated = await Trips.update({id: req.params.id}, req.body)
-//         if(!updated){
-//             return res.status(404).json({ error: "No record found with that ID"})
-//         } else {
-//             res.status(201).json(updated);
-//         }
-        
-//     } catch (error) {
-//         res.status(500).json({message:'internal sever error'})
-//     }
-   
-// })
 server.put('/trips/:id', (req, res) => {
     const id = req.params.id;
     const update = req.body;
@@ -103,8 +89,76 @@ server.post('/trips', async (req, res) => {
 		res.status(500).json({ error: "Internal error." });
 	}
   })
+// TRIP CRUD END // TRIP CRUD END // TRIP CRUD END // TRIP CRUD END // TRIP CRUD END // TRIP CRUD END // TRIP CRUD END 
 
+// PROFILE CRUD START // PROFILE CRUD START // PROFILE CRUD START // PROFILE CRUD START // PROFILE CRUD START // PROFILE CRUD START 
+// GET // GET // GET // GET // GET // GET // GET // GET // GET // GET // GET // GET // GET // GET // GET // GET // GET 
 
+server.get('/profile', async (req, res) => {
+    try {
+        const profile = await Profile.getAll()
+        res.status(200).json(profile)
+    } catch (error) {
+        res.status(500).json({message:'internal server error'})
+    }
+});
+
+server.get('/profile/:id', async (req, res) => {
+    try {
+        const profile = await Profile.getById(id = req.params.id);
+        if(!profile) {
+            return res.status(404).json({error:"No profile found with this id"});
+        } else {
+            return res.status(200).json(profile);
+        }
+    } catch (error) {
+        res.status(500).json(error)
+    }
+});
+
+//   Update //   Update //   Update //   Update //   Update //   Update //   Update //   Update //   Update //   Update 
+server.put('/profile/:id', (req, res) => {
+    const id = req.params.id;
+    const update = req.body;
+  
+    db('profile')
+      .where({ id })
+      .update(update)
+      .then(profile => {
+        res.status(200).json({ profile });
+      })
+      .catch(error => {
+        errorHandler(res, 500, 'There was an error updating the message.', error);
+      });
+  });
+
+// POST // POST // POST // POST // POST // POST // POST // POST // POST // POST // POST // POST // POST // POST // POST 
+server.post('/profile', async (req, res) => {
+    const{name, tagline, age, experience} = req.body;
+    if(!name || !tagline || !age || !experience)
+    return res.status(422).json({message:'you need to provide proper fields'})
+    try {
+        const newProfile = await Profile.insert(req.body);
+        res.status(201).json(newProfile);
+    } catch (error) {
+        res.status(500).json({message:'internal sever error'})
+    }
+   
+  });
+
+//   DELETE //   DELETE //   DELETE //   DELETE //   DELETE //   DELETE //   DELETE //   DELETE //   DELETE //   DELETE 
+server.delete('/profile/:id', async (req, res) => {
+    try {
+		const profile = await Profile.remove(id= req.params.id );
+		if (!profile) {
+			return res.status(404).json({ error: "No trip found for that Id." });
+		} else {
+			return res.status(204).end();
+		}
+	} catch (error) {
+		res.status(500).json({ error: "Internal error." });
+	}
+  })
 
 
 

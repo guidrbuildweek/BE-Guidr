d4f50def9a6e90cee3ab2e59a196fcbd868b2f74
Henry Neal <henry.neal0@gmail.com>
2019-03-11 16:28:28 -0400

    added post route and updated migration tables to non hyphen

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const authRoute = require('../auth/authRoute');
 const helmet = require("helmet");
 const cors = require("cors");
+const Trips = require("./tripModel");
 
 const server = express();
 
@@ -16,6 +17,39 @@ server.get("/", (req, res) => {
   });
 
 server.use('/', authRoute);
+
+server.get('/trips', async (req, res) => {
+    try {
+        const trip = await Trips.getAll()
+        res.status(200).json(rows)
+    } catch (error) {
+        res.status(500).json({message:'internal server error'})
+    }
+});
+
+server.get('/trips/:id', async (req, res) => {
+    try {
+        const trip = await db('trips')
+        .where(({id: req.params.id}))
+        .first();
+        res.status(200).json(trip);
+    } catch (error) {
+        res.status(500).json(error)
+    }
+});
+
+server.post('/trips', async (req, res) => {
+    const{tripType, tripName, durationStart, durationEnd, privPro, datePosted, location,} = req.body;
+    if(!tripType || !tripName || !durationStart || !durationEnd || !privPro || !datePosted || !location)
+    return res.status(422).json({message:'you need to provide proper fields'})
+    try {
+        const newTrip = await trips.insert(req.body);
+        res.status(201).json(newTrip);
+    } catch (error) {
+        res.status(500).json({message:'internal sever error'})
+    }
+   
+  });
 
 
 module.exports = server;
